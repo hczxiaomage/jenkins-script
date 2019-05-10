6464fed5b03f3a7d56ac0dd0ad2841e6aa4c99df
wuzhiming <wuzhiming.chukong@gmail.com>
2019-05-10 14:03:52 +0800

    add fireball host task

--- mac/fireball.groovy
+++ mac/fireball.groovy
@@ -20,6 +20,7 @@ node('mac') {
     stage ('setup environment') {
         if (Boolean.parseBoolean(env.FIREBALL_SETUP_ENV)) {
             sh 'npm install'
+            sh 'gulp update-hosts'
         } else {
             echo 'skip setup-environment stage'
         }

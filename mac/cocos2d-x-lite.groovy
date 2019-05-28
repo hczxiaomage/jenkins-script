e494cdb4b46195326752bdd65e806a5b90cfa1d6
wuzhiming <wuzhiming.chukong@gmail.com>
2019-05-28 15:41:27 +0800

    add -lite publish version check

--- mac/cocos2d-x-lite.groovy
+++ mac/cocos2d-x-lite.groovy
@@ -1,13 +1,15 @@
 properties([parameters([
-  string(name: 'FIREBALL_BUILD_BRANCH', defaultValue: 'v2.0-release', description: '构建的分支(对应GitHub上的branch)'),
+  string(name: 'COCOS2DX_BUILD_BRANCH', defaultValue: 'v2.0-release', description: '构建的分支(对应GitHub上的branch)'),
   booleanParam(name: 'FIREBALL_SETUP_ENV', defaultValue: false, description: '是否初始化环境'),
   booleanParam(name: 'FIREBALL_MAKE_COCOS2DX', defaultValue: false, description: '是否打包上传 cocos2dx 代码'),
   booleanParam(name: 'FIREBALL_LITE_PUBLISH_MAC', defaultValue: true, description:'是否重新构建上传-lite仓库模拟器'),
 ])])
 
+String version = COCOS2DX_BUILD_BRANCH.substring(1,COCOS2DX_BUILD_BRANCH.length());
+
 node('mac') {
     stage ('checkout code'){
-        git branch: "${FIREBALL_BUILD_BRANCH}", url: 'git@github.com:cocos-creator/cocos2d-x-lite.git'
+        git branch: "${COCOS2DX_BUILD_BRANCH}", url: 'git@github.com:cocos-creator/cocos2d-x-lite.git'
     }
 
     stage ('setup environment') {
@@ -22,7 +24,7 @@ node('mac') {
 
     stage ('make cocos2d-x') {
         if (Boolean.parseBoolean(env.FIREBALL_LITE_PUBLISH)) {
-            sh 'gulp make-cocos2d-x -b ${FIREBALL_BUILD_BRANCH}'
+            sh 'gulp make-cocos2d-x -b ${version}'
         } else {
             echo 'skip make cocos2d-x stage'
         }
@@ -30,7 +32,7 @@ node('mac') {
 
     stage ('cocos2d-x-lite publish') {
         if (Boolean.parseBoolean(env.FIREBALL_LITE_PUBLISH_MAC)) {
-            sh 'gulp publish -b ${FIREBALL_BUILD_BRANCH}'
+            sh 'gulp publish -b ${version}'
         } else {
             echo 'skip cocos2d-x-lite publish stage'
         }

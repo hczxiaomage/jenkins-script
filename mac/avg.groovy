e6132142c5755aa1f4b48bb3cfb97c4ab95f50cc
wuzhiming <wuzhiming.chukong@gmail.com>
2019-07-04 10:39:47 +0800

    add avg project

---
+++ mac/avg.groovy
@@ -0,0 +1,27 @@
+properties([parameters([
+  string(name: 'EDITOR_BUILD_BRANCH', defaultValue: 'master', description: '构建的分支(对应GitHub上的branch)'),
+  booleanParam(name: 'EDITOR_SETUP_ENV', defaultValue: false, description: '是否初始化环境')
+])])
+
+node('mac') {
+    stage ('checkout code'){
+        git branch: "${EDITOR_BUILD_BRANCH}", url: 'git@github.com:cocos-creator/avg.git'
+    }
+
+    stage ('setup environment') {
+        if (Boolean.parseBoolean(env.EDITOR_SETUP_ENV)) {
+            dir('avg-electron') {
+                sh 'npm install'
+                sh 'npm install cocos-creator/creator-asar'
+            }
+        } else {
+            echo 'skip setup-environment stage'
+        }
+    }
+
+    stage ('publish editor') {
+        dir('avg-electron') {
+            sh 'gulp publish'
+        }
+    }
+}
---
+++ windows/avg.groovy
@@ -0,0 +1,27 @@
+properties([parameters([
+  string(name: 'EDITOR_BUILD_BRANCH', defaultValue: 'master', description: '构建的分支(对应GitHub上的branch)'),
+  booleanParam(name: 'EDITOR_SETUP_ENV', defaultValue: false, description: '是否初始化环境'),
+])])
+
+node('windows') {
+    stage ('checkout code'){
+        git branch: "${EDITOR_BUILD_BRANCH}", url: 'git@github.com:cocos-creator/editor-3d.git'
+    }
+
+    stage ('setup environment') {
+        if (Boolean.parseBoolean(env.EDITOR_SETUP_ENV)) {
+            dir('avg-electron') {
+                bat 'npm install'
+                bat 'npm install cocos-creator/creator-asar'
+            }
+        } else {
+            echo 'skip setup-environment stage'
+        }
+    }
+
+    stage ('publish editor') {
+        dir('avg-electron') {
+           bat 'gulp publish'
+        }
+    }
+}

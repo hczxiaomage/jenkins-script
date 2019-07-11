fa5f71bbbd68404853892493693691a0381ce0c9
wuzhiming <wuzhiming.chukong@gmail.com>
2019-07-11 10:23:53 +0800

    change default branch

--- mac/avg.groovy
+++ mac/avg.groovy
@@ -1,5 +1,5 @@
 properties([parameters([
-  string(name: 'EDITOR_BUILD_BRANCH', defaultValue: 'master', description: '构建的分支(对应GitHub上的branch)'),
+  string(name: 'EDITOR_BUILD_BRANCH', defaultValue: 'dev', description: '构建的分支(对应GitHub上的branch)'),
   booleanParam(name: 'EDITOR_SETUP_ENV', defaultValue: false, description: '是否初始化环境')
 ])])
 
@@ -11,7 +11,7 @@ node('mac') {
     stage ('setup environment') {
         if (Boolean.parseBoolean(env.EDITOR_SETUP_ENV)) {
             dir('avg-electron') {
-                sh 'npm install'
+                sh 'npm install --production'
                 sh 'npm install cocos-creator/creator-asar'
             }
         } else {
--- windows/avg.groovy
+++ windows/avg.groovy
@@ -1,7 +1,7 @@
 properties([parameters([
-  string(name: 'EDITOR_BUILD_BRANCH', defaultValue: 'master', description: '构建的分支(对应GitHub上的branch)'),
+  string(name: 'EDITOR_BUILD_BRANCH', defaultValue: 'dev', description: '构建的分支(对应GitHub上的branch)'),
   booleanParam(name: 'EDITOR_SETUP_ENV', defaultValue: false, description: '是否初始化环境'),
-  booleanParam(name: 'EDITOR_GREEN', defaultValue: false, description: '是否是构建绿色版本'),
+  booleanParam(name: 'EDITOR_GREEN', defaultValue: true, description: '是否是构建绿色版本'),
   booleanParam(name: 'EDITOR_INSTALLER', defaultValue: false, description: '是否是构建安装版本版本'),
 ])])
 
@@ -13,7 +13,7 @@ node('windows') {
     stage ('setup environment') {
         if (Boolean.parseBoolean(env.EDITOR_SETUP_ENV)) {
             dir('avg-electron') {
-                bat 'npm install'
+                bat 'npm install --production'
                 bat 'npm install cocos-creator/creator-asar'
             }
         } else {

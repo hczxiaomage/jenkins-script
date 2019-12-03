21d3ad96a39cbb4fb0f41bc3206fac201fe956a6
wuzhiming <wuzhiming.chukong@gmail.com>
2019-12-03 10:22:54 +0800

    test build action

--- mac/fireball.groovy
+++ mac/fireball.groovy
@@ -1,47 +1,47 @@
-pipeline {
-    properties([parameters([
-      string(name: 'FIREBALL_BUILD_BRANCH', defaultValue: 'v2.2.0-release', description: '构建的分支(对应GitHub上的branch)'),
-      string(name: 'FIREBALL_PUBLISH_VERSION', defaultValue: '2.2.0', description: '用户实际看到的版本号'),
-      booleanParam(name: 'FIREBALL_HIDE_VERSION_CODE', defaultValue: false, description: '是否隐藏版本号'),
-      booleanParam(name: 'FIREBALL_UPLOAD_WAN', defaultValue: false, description: '是否上传到外网'),
-      booleanParam(name: 'FIREBALL_SKIP_NPM_REBUILD', defaultValue: false, description: '是否跳过 npm install 和 npm rebuild'),
-      booleanParam(name: 'FIREBALL_SETUP_ENV', defaultValue: false, description: '是否初始化环境'),
-      booleanParam(name: 'FIREBALL_UPDATE_FIREBALL', defaultValue: true, description: 'update fireball'),
-      booleanParam(name: 'FIREBALL_UPDATE_BUILTIN', defaultValue: true, description: '是否更新built-in'),
-      booleanParam(name: 'FIREBALL_CHECKOUT_SETTING_BRANCH', defaultValue: true, description: '是否迁出对应版本的分支'),
-      booleanParam(name: 'FIREBALL_UPDATE_HOSTS', defaultValue: true, description: '是否更新hosts'),
-      booleanParam(name: 'FIREBALL_CLEAN_CACHE', defaultValue: true, description: '是否清除缓存'),
-      booleanParam(name: 'FIREBALL_SYNC_ENGINE_VERSION', defaultValue: true, description: '是否同步引擎版本'),
-      booleanParam(name: 'FIREBALL_UPDATE_EXTERNS', defaultValue: true, description: '是否更新externs'),
-      booleanParam(name: 'FIREBALL_UPDATE_TEMPLATES', defaultValue: true, description: '是否更新新建工程的模板'),
-      booleanParam(name: 'FIREBALL_PUSH_TAG', defaultValue: true, description: '是否添加tag'),
-    ])])
-    
-    String paramStr = Boolean.parseBoolean(env.FIREBALL_HIDE_VERSION_CODE)? ' -B ':' -b ';
-    paramStr += env.FIREBALL_PUBLISH_VERSION;
-    
-    if (Boolean.parseBoolean(env.FIREBALL_UPLOAD_WAN)) {
-        paramStr += ' --fw'
+properties([parameters([
+  string(name: 'FIREBALL_BUILD_BRANCH', defaultValue: 'v2.2.0-release', description: '构建的分支(对应GitHub上的branch)'),
+  string(name: 'FIREBALL_PUBLISH_VERSION', defaultValue: '2.2.0', description: '用户实际看到的版本号'),
+  booleanParam(name: 'FIREBALL_HIDE_VERSION_CODE', defaultValue: false, description: '是否隐藏版本号'),
+  booleanParam(name: 'FIREBALL_UPLOAD_WAN', defaultValue: false, description: '是否上传到外网'),
+  booleanParam(name: 'FIREBALL_SKIP_NPM_REBUILD', defaultValue: false, description: '是否跳过 npm install 和 npm rebuild'),
+  booleanParam(name: 'FIREBALL_SETUP_ENV', defaultValue: false, description: '是否初始化环境'),
+  booleanParam(name: 'FIREBALL_UPDATE_FIREBALL', defaultValue: true, description: 'update fireball'),
+  booleanParam(name: 'FIREBALL_UPDATE_BUILTIN', defaultValue: true, description: '是否更新built-in'),
+  booleanParam(name: 'FIREBALL_CHECKOUT_SETTING_BRANCH', defaultValue: true, description: '是否迁出对应版本的分支'),
+  booleanParam(name: 'FIREBALL_UPDATE_HOSTS', defaultValue: true, description: '是否更新hosts'),
+  booleanParam(name: 'FIREBALL_CLEAN_CACHE', defaultValue: true, description: '是否清除缓存'),
+  booleanParam(name: 'FIREBALL_SYNC_ENGINE_VERSION', defaultValue: true, description: '是否同步引擎版本'),
+  booleanParam(name: 'FIREBALL_UPDATE_EXTERNS', defaultValue: true, description: '是否更新externs'),
+  booleanParam(name: 'FIREBALL_UPDATE_TEMPLATES', defaultValue: true, description: '是否更新新建工程的模板'),
+  booleanParam(name: 'FIREBALL_PUSH_TAG', defaultValue: true, description: '是否添加tag'),
+])])
+
+String paramStr = Boolean.parseBoolean(env.FIREBALL_HIDE_VERSION_CODE)? ' -B ':' -b ';
+paramStr += env.FIREBALL_PUBLISH_VERSION;
+
+if (Boolean.parseBoolean(env.FIREBALL_UPLOAD_WAN)) {
+    paramStr += ' --fw'
+}
+if (Boolean.parseBoolean(env.FIREBALL_SKIP_NPM_REBUILD)) {
+    paramStr += ' --sn'
+}
+
+env.PARAM_STRING = paramStr;
+
+def execGulp(taskName) {
+    String command = 'gulp ' + taskName + ' ' + env.PARAM_STRING;
+
+    echo 'exec command ' + command;
+    if (isUnix()) {
+        sh command;
+    } else {
+        bat command;
     }
-    if (Boolean.parseBoolean(env.FIREBALL_SKIP_NPM_REBUILD)) {
-        paramStr += ' --sn'
-    }
-    
-    env.PARAM_STRING = paramStr;
-    
-    def execGulp(taskName) {
-        String command = 'gulp ' + taskName + ' ' + env.PARAM_STRING;
-    
-        echo 'exec command ' + command;
-        if (isUnix()) {
-            sh command;
-        } else {
-            bat command;
-        }
-    }
-    
-    node('mac') {
-        stage ('checkout code') {
+}
+
+node('mac') {
+try {
+    stage ('checkout code') {
             git branch: "${FIREBALL_BUILD_BRANCH}", url: 'git@github.com:cocos-creator/fireball.git'
         }
     
@@ -147,11 +147,8 @@ pipeline {
         stage ('make dist and deploy') {
             execGulp('make-dist-and-deploy');
         }
+    }catch(e){
+        echo 'build fail'
     }
-    
-    post {
-            always {
-                echo 'I will always say Hello again!'
-            }
-    }
+   
 }

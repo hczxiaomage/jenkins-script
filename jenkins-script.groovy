955412c06557906b8b3ef226f1e28b144849ab8d
wuzhiming <wuzhiming.chukong@gmail.com>
2019-12-20 09:48:40 +0800

    update jenkins-script 2

--- jenkins-script.groovy
+++ jenkins-script.groovy
@@ -1,7 +1,11 @@
 //用于添加 jenkins-script 到对应节点的脚本
-node ('mac') {
-    git branch: 'creator', url: 'git@github.com:wuzhiming/jenkins-script.git'
+properties([parameters([
+    booleanParam(name: 'IS_WINDOWS', defaultValue: false, description: '是否是 windows 节点'),
+])])
+def nodeName = 'mac'
+if(Boolean.parseBoolean(env.IS_WINDOWS)) {
+    nodeName = 'windows'
 }
-node ('windows') {
+node (nodeName) {
     git branch: 'creator', url: 'git@github.com:wuzhiming/jenkins-script.git'
 }
